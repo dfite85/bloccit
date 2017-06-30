67daccf97b4d16004cccbae7f6124b82c37e2361
Dustin <dfite85@gmail.com>
2017-06-30 01:19:28 +0000

    repair to setup

--- app/controllers/welcom_controller.rb
+++
@@ -1,7 +0,0 @@
-class WelcomController < ApplicationController
-  def index
-  end
-
-  def about
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'welcom/index'
+  get 'welcome/index'
 
-  get 'welcom/about'
+  get 'welcome/about'
+  
+  root 'welcome#index'
+end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -57,4 +60,4 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
+
