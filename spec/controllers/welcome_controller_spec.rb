c0208912de346b4477c8eb2d61e800cb4708ef9c
Dustin <dfite85@gmail.com>
2017-07-11 00:34:58 +0000

    working files after lesson 12 assingment

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -14,4 +14,11 @@ RSpec.describe WelcomeController, type: :controller do
             expect(response).to render_template("about")                        #expects the controllers response to render the About template
         end
     end
+    
+    describe "GET faq" do
+        it "renders the faq template" do
+            get :faq
+            expect(response).to render_template("faq")
+        end
+    end
 end
