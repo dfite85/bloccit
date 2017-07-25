087fb7d333820f73032c3585eaf86dbd80beca8c
Dustin <dfite85@gmail.com>
2017-07-25 00:21:53 +0000

    Working files after lesson 16 assingment

--- db/seeds.rb
+++ db/seeds.rb
@@ -28,6 +28,10 @@ posts = Post.all
     )
 end
 
+puts "#{Post.count}"
+Post.find_or_create_by(title: "Random ass title", body: "Random ass body")
+puts "{Post.count}"
+
 puts "Seed finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
