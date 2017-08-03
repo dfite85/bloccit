169b8ff05a816ed58b68742ea972fa1ea464853b
Dustin <dfite85@gmail.com>
2017-08-03 18:00:10 +0000

    Working files before lesson 19 assingment

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -15,6 +15,61 @@ RSpec.describe PostsController, type: :controller do
       expect(assigns(:posts)).to eq([my_post])
     end
   end
+  
+  describe "GET new" do
+    it "returns https success" do
+      get :new                                                                  #when new is invoked a new and unsaved Post obj is created
+      expect(response).to have_http_status(:success)
+    end
+    
+    it "renders the #new view" do                                               #verifies that the new renders the correct template
+      get :new
+      expect(response).to render_template :new                                  #Questions for k-dog, why is there now () around : new here
+    end
+    
+    it "instantiates @post" do                                                  #expect the @post inst var to be init by PostsContrller#new and assings gives us access to the @post var assiningin it to :post
+      get :new
+      expect(assigns(:post)).not_to be_nil
+    end
+  end
+
+  describe "Post create" do
+    
+    it "increases the number of Post by 1" do                                   #expect after PostsContorller#create is called wih parm the ocunt will increase by one
+      expect{post :create, post: {title: RandomData.random_sentence, body: RandomData.random_paragraph}}.to change(Post,:count).by(1)
+    end
+    
+    it "assigns the new post to @post" do                                       #when create is posted we expect post to be assinged to @post
+      post :create, post: {title: RandomData.random_sentence, body: RandomData.random_paragraph}
+      expect(assigns(:post)).to eq Post.last
+    end
+    
+    it "redirects to the new post" do                                           #expect to be redirected to the new post
+      post :create, post: {title: RandomData.random_sentence, body: RandomData.random_paragraph}
+      expect(response).to redirect_to Post.last
+    end
+  end
+  
+  describe "GET show" do
+    it "returns http success" do
+      get :show, {id: my_post.id}
+      expect(response).to have_http_status(:success)
+    end
+    
+    it "renders the #show view" do
+      get :show, {id: my_post.id}
+      expect(response).to render_template :show
+    end
+    
+    it "assigns my_post to @post" do
+      get :show, {id: my_post.id}
+      expect(assigns(:post)).to eq(my_post)
+    end
+  end
+end
+  
+    
+  
 =begin                                                                          #ty ruby docs!!
   describe "GET #show" do
     it "returns http success" do
@@ -23,12 +78,7 @@ RSpec.describe PostsController, type: :controller do
     end
   end
 
-  describe "GET #new" do
-    it "returns http success" do
-      get :new
-      expect(response).to have_http_status(:success)
-    end
-  end
+
 
   describe "GET #edit" do
     it "returns http success" do
@@ -37,6 +87,6 @@ RSpec.describe PostsController, type: :controller do
     end
   end
 =end
-end
+
 
 
