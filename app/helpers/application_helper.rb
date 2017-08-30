1cb482b1f7f3e2ebb12476d899ba7969f59f3fd8
Dustin <dfite85@gmail.com>
2017-08-30 19:13:45 +0000

    working topic files post still jacked up

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
     def form_group_tag(errors, &block)
         css_class = 'from-group'
-        css_class = ' has-error' if errors.any?
+        css_class << ' has-error' if errors.any?
         
-        content_tag :div, caputre(&block), class: css_class
+        content_tag :div, capture(&block), class: css_class
     end
     
 end
